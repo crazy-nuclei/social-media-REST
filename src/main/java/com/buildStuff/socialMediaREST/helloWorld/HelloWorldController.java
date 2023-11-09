86b440f212b23359ec7e2e850148ba8e78469213
Aayush Agarwal <crazy_nuclei@localhost>
2023-11-09 15:02:35 +0530

    Using path variable with hello world

--- src/main/java/com/buildStuff/socialMediaREST/helloWorld/HelloWorldController.java
+++ src/main/java/com/buildStuff/socialMediaREST/helloWorld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.buildStuff.socialMediaREST.helloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +15,11 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldUsingBean() {
 		return new HelloWorldBean("Hello World Bean");
+	}
+	
+	@GetMapping("/hello-world/pathVariable/{name}")
+	public String helloWorldPathVariable(@PathVariable String name) {
+		return "Hello World, " + name;
 	}
 }
 
