fdfe30676dac2ac15fb480692ac718ced049ef1c
Aayush Agarwal <crazy_nuclei@localhost>
2023-11-09 14:44:07 +0530

    Initial Commit

---
+++ src/main/java/com/buildStuff/socialMediaREST/SocialMediaRestApplication.java
@@ -0,0 +1,13 @@
+package com.buildStuff.socialMediaREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildStuff/socialMediaREST/SocialMediaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildStuff.socialMediaREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
